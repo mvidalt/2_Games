2c3074eb87c40d765ec461665269c52e62427547
mvidalt <mvidalt@cifpfbmoll.eu>
2023-11-14 15:19:09 +0100

    Añadiendo la tabla

--- app/src/main/java/com/example/a2games/Game1.java
+++ app/src/main/java/com/example/a2games/Game1.java
@@ -1,4 +1,14 @@
 package com.example.a2games;
 
-public class Game1 {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
