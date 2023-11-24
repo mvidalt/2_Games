d813252b2f7335f9b93d9a3737357cd1d3e7d935
mvidalt <mvidalt@cifpfbmoll.eu>
2023-11-24 01:03:14 +0100

    Fling

--- app/src/main/java/com/example/a2games/Game1.java
+++ app/src/main/java/com/example/a2games/Game1.java
@@ -3,16 +3,19 @@ package com.example.a2games;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
-public class Game1 extends AppCompatActivity {
+public class Game1 extends AppCompatActivity implements GestureDetector.OnGestureListener {
     private static final int NUM_BUTTONS = 16;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,5 +51,60 @@ public class Game1 extends AppCompatActivity {
     public void goBack(View view) {
         Intent goBack = new Intent(this, MainActivity.class);
         startActivity(goBack);
+    }
+
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float velocityX, float velocityY) {
+        float diffY = motionEvent2.getY() - motionEvent1.getY();
+        float diffX = motionEvent2.getX() - motionEvent1.getX();
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (Math.abs(diffX) > 100 && Math.abs(velocityX) > 100) {
+                if (diffX > 0) {
+                    // Deslizamiento hacia la derecha
+                    Log.d("Mensaje","derecha");
+                } else {
+                    // Deslizamiento hacia la izquierda
+                    Log.d("Mensaje","izquierda");
+                }
+            }
+        } else {
+            if (Math.abs(diffY) > 100 && Math.abs(velocityY) > 100) {
+                if (diffY > 0) {
+                    // Deslizamiento hacia abajo
+                    Log.d("Mensaje","abajo");
+                } else {
+                    // Deslizamiento hacia arriba
+                    Log.d("Mensaje","arriba");
+                }
+            }
+        }
+
+        return true;
     }
 }
