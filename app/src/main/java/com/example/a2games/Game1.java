e586367edacf25d10061685deb1d1016ef625da2
mvidalt <mvidalt@cifpfbmoll.eu>
2023-12-14 14:02:28 +0100

    2048 por codigo - base

--- app/src/main/java/com/example/a2games/Game1.java
+++ app/src/main/java/com/example/a2games/Game1.java
@@ -1,64 +1,41 @@
 package com.example.a2games;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 
 import java.util.Random;
 
 public class Game1 extends AppCompatActivity implements GestureDetector.OnGestureListener {
-    private static final int NUM_BUTTONS = 16;
-    private GestureDetector gestureDetector;
 
-    private static final int NUM_ROWS = 4;
-    private static final int NUM_COLS = 4;
-    private Button[][] buttonsGrid = new Button[NUM_ROWS][NUM_COLS];
+    private GridLayout gridLayout;
+    private Button[][] buttonsArray;
+
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game1_layout);
-        Random random = new Random();
-        int randomButton1 = random.nextInt(NUM_BUTTONS) + 1;
-        int randomButton2 = random.nextInt(NUM_BUTTONS) + 1;
 
-        while (randomButton2 == randomButton1) {
-            randomButton2 = random.nextInt(NUM_BUTTONS) + 1;
-        }
-
-        Button[] buttons = new Button[NUM_BUTTONS];
-        for (int i = 0; i < NUM_BUTTONS; i++) {
-            String buttonId = "button" + (i + 1);
-            int buttonResId = getResources().getIdentifier(buttonId, "id", getPackageName());
-            buttons[i] = findViewById(buttonResId);
-        }
-
-        buttons[randomButton1 - 1].setVisibility(View.VISIBLE);
-        buttons[randomButton2 - 1].setVisibility(View.VISIBLE);
         gestureDetector = new GestureDetector(this, this);
 
-        int buttonCounter = 0;
-        for (int row = 0; row < NUM_ROWS; row++) {
-            for (int col = 0; col < NUM_COLS; col++) {
-                String buttonId = "button" + (++buttonCounter);
-                int buttonResId = getResources().getIdentifier(buttonId, "id", getPackageName());
-                buttonsGrid[row][col] = findViewById(buttonResId);
-            }
-        }
-
-        for (int row = 0; row < NUM_ROWS; row++) {
-            for (int col = 0; col < NUM_COLS; col++) {
-                Button button = buttonsGrid[row][col];
-                String buttonText = button.getText().toString(); // Obtener texto del botón si tiene alguno
-                Log.d("ArrayContent", "Button at row " + row + ", col " + col + ": " + buttonText);
-            }
-        }
+        gridLayout = findViewById(R.id.gridLayout);
+        createGameButtons();
+        // Inicializar el array de botones
+        buttonsArray = new Button[4][4];
 
         Button buttonNewGame = findViewById(R.id.buttonNewGame);
         buttonNewGame.setOnClickListener(v -> {
@@ -67,6 +44,52 @@ public class Game1 extends AppCompatActivity implements GestureDetector.OnGestur
             Intent intent = new Intent(Game1.this, Game1.class);
             startActivity(intent);
         });
+    }
+
+    private void createGameButtons() {
+        // Crear botones y agregarlos a la cuadrícula
+        final int rowCount = 4;
+        final int columnCount = 4;
+        final int margin = 16; // Define el margen vertical entre los botones
+
+        // Crear un arreglo bidimensional de botones para almacenar referencias
+        Button[][] buttons = new Button[rowCount][columnCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < columnCount; j++) {
+                Button button = new Button(this);
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams(
+                        GridLayout.spec(i), GridLayout.spec(j));
+
+                // Ajustar el margen vertical si es la primera fila
+                if (i == 0) {
+                    params.setMargins(0, margin, 0, 0);
+                } else {
+                    params.setMargins(0, 0, 0, 0);
+                }
+
+                button.setLayoutParams(params);
+
+                ColorStateList colorStateList = ColorStateList.valueOf(Color.parseColor("#eee4da"));
+                button.setText("0");
+                button.setBackgroundTintList(colorStateList);
+                button.setTextColor(Color.parseColor("#776e65"));
+                button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+                button.setAllCaps(false);
+
+                gridLayout.addView(button);
+
+                // Almacenar referencias a los botones en la matriz
+                buttonsArray[i][j] = button;
+            }
+        }
+
+
+    }
+
+    public void goBack(View view) {
+        Intent IntentMain = new Intent(this, MainActivity.class);
+        startActivity(IntentMain);
     }
 
     @Override
@@ -112,167 +135,61 @@ public class Game1 extends AppCompatActivity implements GestureDetector.OnGestur
             if (velocityX > 0) {
                 // Fling hacia la derecha
                 Log.d("Gesto", "Fling hacia la derecha");
-                boolean moved = false;
-                int[] row = new int[2];
-                int[] col = new int[2];
-
-                int visibleCount = 0;
-                outerloop:
-                for (int r = 0; r < NUM_ROWS; r++) {
-                    for (int c = 0; c < NUM_COLS; c++) {
-                        if (buttonsGrid[r][c].getVisibility() == View.VISIBLE) {
-                            row[visibleCount] = r;
-                            col[visibleCount] = c;
-                            visibleCount++;
-                            if (visibleCount == 2) {
-                                break outerloop;
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < 2; i++) {
-                    while (col[i] < NUM_COLS - 1) {
-                        buttonsGrid[row[i]][col[i]].setVisibility(View.INVISIBLE);
-                        col[i]++;
-                        buttonsGrid[row[i]][col[i]].setVisibility(View.VISIBLE);
-                        moved = true;
-                    }
-                }
-
-
-
+                moveRight();
+                printButtonsArray();
             } else {
                 // Fling hacia la izquierda
                 Log.d("Gesto", "Fling hacia la izquierda");
-                boolean moved = false;
-
-                do {
-                    moved = false;
-
-                    // Encuentra la posición actual de los botones visibles en la cuadrícula
-                    int[] row = new int[2];
-                    int[] col = new int[2];
-
-                    int visibleCount = 0;
-                    outerloop:
-                    for (int r = 0; r < NUM_ROWS; r++) {
-                        for (int c = 0; c < NUM_COLS; c++) {
-                            if (buttonsGrid[r][c].getVisibility() == View.VISIBLE) {
-                                row[visibleCount] = r;
-                                col[visibleCount] = c;
-                                visibleCount++;
-                                if (visibleCount == 2) {
-                                    break outerloop;
-                                }
-                            }
-                        }
-                    }
-
-                    // Verifica si los botones pueden moverse hacia la izquierda
-                    if (col[0] > 0 && col[1] > 0) {
-                        // Oculta los botones en sus posiciones actuales
-                        for (int i = 0; i < 2; i++) {
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.INVISIBLE);
-                        }
-
-                        // Actualiza la posición de los botones hacia la izquierda
-                        for (int i = 0; i < 2; i++) {
-                            col[i]--;
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.VISIBLE);
-                            moved = true; // Indica que al menos uno de los botones se ha movido
-                        }
-                    }
-                } while (moved); // Continúa moviendo mientras al menos uno de los botones se mueva
             }
         } else {
             // Fling vertical
             if (velocityY > 0) {
                 // Fling hacia abajo
                 Log.d("Gesto", "Fling hacia abajo");
-                boolean moved = false;
-
-                do {
-                    moved = false;
-
-                    int[] row = new int[2];
-                    int[] col = new int[2];
-
-                    int visibleCount = 0;
-                    outerloop:
-                    for (int r = 0; r < NUM_ROWS; r++) {
-                        for (int c = 0; c < NUM_COLS; c++) {
-                            if (buttonsGrid[r][c].getVisibility() == View.VISIBLE) {
-                                row[visibleCount] = r;
-                                col[visibleCount] = c;
-                                visibleCount++;
-                                if (visibleCount == 2) {
-                                    break outerloop;
-                                }
-                            }
-                        }
-                    }
-
-                    if (row[0] < NUM_ROWS - 1 && row[1] < NUM_ROWS - 1) {
-                        for (int i = 0; i < 2; i++) {
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.INVISIBLE);
-                        }
-
-                        for (int i = 0; i < 2; i++) {
-                            row[i]++;
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.VISIBLE);
-                            moved = true;
-                        }
-                    }
-                } while (moved);
-
             } else {
                 // Fling hacia arriba
                 Log.d("Gesto", "Fling hacia arriba");
-                boolean moved = false;
-
-                do {
-                    moved = false;
-
-                    int[] row = new int[2];
-                    int[] col = new int[2];
-
-                    int visibleCount = 0;
-                    outerloop:
-                    for (int r = 0; r < NUM_ROWS; r++) {
-                        for (int c = 0; c < NUM_COLS; c++) {
-                            if (buttonsGrid[r][c].getVisibility() == View.VISIBLE) {
-                                row[visibleCount] = r;
-                                col[visibleCount] = c;
-                                visibleCount++;
-                                if (visibleCount == 2) {
-                                    break outerloop;
-                                }
-                            }
-                        }
-                    }
-
-                    if (row[0] > 0 && row[1] > 0) {
-                        for (int i = 0; i < 2; i++) {
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.INVISIBLE);
-                        }
-
-                        for (int i = 0; i < 2; i++) {
-                            row[i]--;
-                            buttonsGrid[row[i]][col[i]].setVisibility(View.VISIBLE);
-                            moved = true;
-                        }
-                    }
-                } while (moved);
             }
         }
-
         return true;
     }
 
-    public void goBack(View view){
-        Intent IntentMain = new Intent(this,MainActivity.class);
-        startActivity(IntentMain);
+    private void moveRight() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 2; j >= 0; j--) {
+                if (buttonsArray[i][j] != null && !buttonsArray[i][j].getText().equals("0")) {
+                    int column = j;
+                    while (column < 3) {
+                        int nextColumn = column + 1;
+                        if (buttonsArray[i][nextColumn].getText().equals("0")) {
+                            buttonsArray[i][nextColumn].setText(buttonsArray[i][column].getText());
+                            buttonsArray[i][column].setText("0");
+                            column = nextColumn;
+                        } else if (buttonsArray[i][nextColumn].getText().equals(buttonsArray[i][column].getText())) {
+                            int value = Integer.parseInt(buttonsArray[i][column].getText().toString()) * 2;
+                            buttonsArray[i][nextColumn].setText(String.valueOf(value));
+                            buttonsArray[i][column].setText("0");
+                            break;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void printButtonsArray() {
+        for (int i = 0; i < buttonsArray.length; i++) {
+            for (int j = 0; j < buttonsArray[i].length; j++) {
+                if (buttonsArray[i][j] != null) {
+                    String buttonText = buttonsArray[i][j].getText().toString();
+                    Log.d("Button", "Button at [" + i + "][" + j + "]: " + buttonText);
+                } else {
+                    Log.d("Button", "Button at [" + i + "][" + j + "]: null");
+                }
+            }
+        }
     }
 
 }
